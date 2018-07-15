71dc725fc96d54ca42b7c5918d2c05e8062fb20d
premasunder <premasunder@gmail.com>
2018-07-15 17:52:09 +0200

    new app shop and cart added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'personal',
     'blog',
     'django.contrib.admin',
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+
+CART_SESSION_ID = 'cart'
